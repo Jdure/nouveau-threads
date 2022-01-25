40ccc5d3eaa3b004ab2f801b0a48403f468ac448
“Jdure” <22172148+Jdure@users.noreply.github.com>
2022-01-24 22:27:42 -0500

    changed the create care query; added swr

--- utils/shopify.tsx
+++ utils/shopify.tsx
@@ -57,48 +57,16 @@ query Products {
 }
             `
 
-export const createCartWithLineItem = gql`
-mutation createCart($cartInput: CartInput) {
-  cartCreate(input: $cartInput) {
+export const createCart = gql`
+mutation cartCreate {
+  cartCreate {
     cart {
       id
-      createdAt
-      updatedAt
-      lines(first:10) {
-        edges {
-          node {
-            id
-            merchandise {
-              ... on ProductVariant {
-                id
-              }
-            }
-          }
-        }
-
-      }
-      attributes {
-        key
-        value
-      }
-      estimatedCost {
-        totalAmount {
-          amount
-          currencyCode
-        }
-        subtotalAmount {
-          amount
-          currencyCode
-        }
-        totalTaxAmount {
-          amount
-          currencyCode
-        }
-        totalDutyAmount {
-          amount
-          currencyCode
-        }
-      }
+    }
+    userErrors {
+      code
+      field
+      message
     }
   }
 }
