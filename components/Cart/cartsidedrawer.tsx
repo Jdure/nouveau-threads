e1215dad76cb06a06e045c2c2584914a9875f309
“Jdure” <22172148+Jdure@users.noreply.github.com>
2022-01-25 22:51:31 -0500

    created todos

--- components/Cart/cartsidedrawer.tsx
+++ components/Cart/cartsidedrawer.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+//TODO: Add drawer transition
+// TODO: Fetch cart items from storefront API
 export default function CartSideDrawer (){
     return (
         <div className="relative h-full w-full ">
@@ -7,7 +9,7 @@ export default function CartSideDrawer (){
         <div className='container bg-stone-100 rounded h-96 w-96 shadow'>
         <h1 className='flex flex-col pl-4 text-2xl font-bold'>Cart</h1> 
         <nav className="mt-10 px-6 ">
-              <a className="hover:text-white hover:bg-emerald-300 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg " href="#">
+              <a className="hover:text-white hover:bg-emerald-300 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg" href="#">
                 <span className="mx-4 text-2xl font-normal">
                   Element
                 </span>
