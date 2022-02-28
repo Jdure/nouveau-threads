0842db695340c65bc3179178e8f337e99785b75c
“Jdure” <22172148+Jdure@users.noreply.github.com>
2022-02-27 21:54:19 -0500

    FIXME undefined fetches data appears after refetch

--- components/Cart/cartsidedrawer.tsx
+++ components/Cart/cartsidedrawer.tsx
@@ -4,16 +4,22 @@ import { useAppContext } from "../../context/AppContext";
 import { useState } from "react";
 import { Edge, GetCart } from "../../types/cart-get";
 
+//NOTE: Fetch Data is typed GetCart or undefined - so some refetches result in undefined
+// Then a refetch happens then the data is there or 
+// so every time the cart drawer is open the app refetches with undefined then the results
+// FIXME: Need to fix this - I should start here https://tkdodo.eu/blog/react-query-and-type-script
+
 export default function CartSideDrawer() {
   const cartData = useAppContext();
   const cartID = cartData?.id;
   const checkoutLink = cartData?.checkoutUrl;
   const [cart, setCart] = useState<GetCart>();
-  const { data, isError, error, isLoading } = useQuery<GetCart>(
+  const { data, isError, error, isFetching } = useQuery<GetCart>(
     ["cart-items", cartID],
     () => retrieveCart(cartID),
     {
       onSuccess: () => setCart(data),
+      initialData: cart,
     }
   );
   const userCart = cart?.data.cart;
@@ -27,40 +33,46 @@ export default function CartSideDrawer() {
         </div>
       ) : (
         <div className="absolute top-0 right-0">
-          <div className="container bg-stone-100 rounded h-screen w-96 shadow">
+          <div className="container bg-stone-100 rounded h-96 w-96 shadow">
             <h1 className="flex flex-col pl-4 text-2xl text-black font-bold">
               Cart
             </h1>
             <div className="mt-10 px-6">
-              <ul className="flex flex-col">
-                {userCart?.lines.edges.map((item: Edge) => {
-                  const articles = item.node;
-                  const articleDetail = articles.merchandise.product;
-                  return (
-                    <li
-                      key={articleDetail.handle}
-                      className="border-gray-400 flex flex-row mb-2"
-                    >
-                      <div className="shadow border select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
-                        <div className="flex flex-col w-10 h-10 justify-center items-center mr-4">
-                          <a href="#" className="block relative">
-                            <img
-                              alt={articleDetail.handle}
-                              src={articleDetail.featuredImage.url}
-                              className="mx-auto object-cover rounded-full h-10 w-10 "
-                            />
-                          </a>
-                        </div>
-                        <div className="flex-1 pl-1 md:mr-16">
-                          <div className="font-medium dark:text-white">
-                            {articleDetail.title}
+              {isFetching ? (
+                <p className="flex flex-col pl-4 text-2xl text-black font-bold">
+                  Fetching...
+                </p>
+              ) : (
+                <ul className="flex flex-col">
+                  {userCart?.lines.edges.map((item: Edge) => {
+                    const articles = item.node;
+                    const articleDetail = articles.merchandise.product;
+                    return (
+                      <li
+                        key={articleDetail.handle}
+                        className="border-gray-400 flex flex-row mb-2"
+                      >
+                        <div className="shadow border select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
+                          <div className="flex flex-col w-10 h-10 justify-center items-center mr-4">
+                            <a href="#" className="block relative">
+                              <img
+                                alt={articleDetail.handle}
+                                src={articleDetail.featuredImage.url}
+                                className="mx-auto object-cover rounded-full h-10 w-10 "
+                              />
+                            </a>
+                          </div>
+                          <div className="flex-1 pl-1 md:mr-16">
+                            <div className="font-medium dark:text-white">
+                              {articleDetail.title}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
             </div>
           </div>
         </div>
