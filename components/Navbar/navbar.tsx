88b79fc034bd76a3834402d1bd250ce6d165fe1c
“Jdure” <22172148+Jdure@users.noreply.github.com>
2022-03-02 15:34:18 -0500

    removed daisy UI, added new nav

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,51 +1,39 @@
-import Link from "next/link"
+import Link from "next/link";
 import { useState } from "react";
-import CartToggleBtn from "../Cart/carttogglebtn"
+import CartToggleBtn from "../Cart/carttogglebtn";
 
 interface NavProps {
-    children?: React.ReactNode
-    toggleFunc: () => void;
+  children?: React.ReactNode;
+  toggleFunc: () => void;
 }
 
-export default function Navbar ({toggleFunc} : NavProps){
-
-    return (
+export default function Navbar({ toggleFunc }: NavProps) {
+  return (
     // Nav title
-<div data-theme="light" className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
-    <div className="flex-none px-2 mx-2">
-        <span className="text-lg font-bold">
-            W.E Are Inspired
-        </span>
-    </div>
-    {/* Nav Links */}
-    <div className="flex-1 px-2 mx-2">
-        <div className="items-stretch hidden lg:flex">
-            <Link href="/" >
-            <a className="btn btn-ghost btn-sm rounded-btn">
-                Home
-            </a>
-            </Link>
-            <Link href="/product">
-            <a className="btn btn-ghost btn-sm rounded-btn">
-                Product
-            </a>
-            </Link>
-            <Link href="/about">
-            <a className="btn btn-ghost btn-sm rounded-btn">
-                About
-            </a>
-            </Link>
-            <Link href="/request">
-            <a className="btn btn-ghost btn-sm rounded-btn">
-                Request
-            </a>
-            </Link>
-        </div>
-    </div>
- 
-    <div className="flex-none navbar-end">
-            <CartToggleBtn onCartToggle={toggleFunc} />
-    </div>
-</div>
-)
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <span className="ml-3 text-xl">W.E Are Inspired</span>
+        </a>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <Link href="/">
+            <a className="mr-5 hover:text-gray-900">Home</a>
+          </Link>
+          <Link href="/product">
+            <a className="mr-5 hover:text-gray-900">Product</a>
+          </Link>
+          <Link href="/about">
+            <a className="mr-5 hover:text-gray-900">About</a>
+          </Link>
+          <Link href="/request">
+            <a className="mr-5 hover:text-gray-900">Request</a>
+          </Link>
+        </nav>
+        {/* Cart Toggle */}
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          <CartToggleBtn onCartToggle={toggleFunc} />
+        </button>
+      </div>
+    </header>
+  );
 }
