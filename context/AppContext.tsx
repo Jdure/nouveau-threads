66d2de2decc6aa3f52dd04d42d1e1c0a220aa324
“Jdure” <22172148+Jdure@users.noreply.github.com>
2022-04-25 15:03:41 -0400

    Added cart to local storage

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useMemo, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { useQuery } from "react-query";
 import { createCartID } from "../components/Cart/cart-api";
 
@@ -18,10 +18,31 @@ const getCartID = () =>
     refetchOnWindowFocus: false,
   });
 
+// - [ ] Save Cart Session on page return from checkout if no purchase was made
+//  - [X] Check if local storage is empty - doesn't have the cart ID
+//  - [ ] If its not empty use the cartID instead of refetching for a new one
+//  - [ ] If empty save the cartID in local storage
+
 const AppContext = createContext<CartContextProps | undefined>(undefined);
 
 export function AppWrapper({ children }: ContextProps) {
+  const [localCart, setLocalCart] = useState<String | null>("");
   const { data } = getCartID();
+
+  // useEffect(() => {
+  //   localStorage.setItem("CART", JSON.stringify(data));
+  // }, [data]);
+
+  useEffect(() => {
+    if (localCart == null) {
+      localStorage.setItem("CART", JSON.stringify(data));
+    }
+  }, [data]);
+
+  useEffect(() => {
+    const cartData = localStorage.getItem("CART");
+    setLocalCart(cartData);
+  }, []);
 
   const contextValue = useMemo(() => {
     return data;
