d9d4c7164fb91386fde00f000b96c01c5a8cc2be
Jdure <22172148+Jdure@users.noreply.github.com>
2022-08-24 09:14:16 -0400

    fix responsive images on mobile

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -21,41 +21,39 @@ export default function StoreProducts() {
       </Head>
       <Jumbotron title="Products" />
       <div className="flex flex-col items-center justify-center">
-        <main className="flex flex-col items-start justify-center w-full flex-1 px-20 ">
-          <div>
-            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-              <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                {data.data.products.edges.map((item: Edge) => {
-                  const product = item.node;
-                  const image = product.featuredImage;
-                  const price = product.priceRange.minVariantPrice;
-                  return (
-                    <Link
-                      key={product.handle}
-                      href={`/product/${product.handle}`}
-                    >
-                      <a className="group border-b-2 bg-teal-50 rounded-md hover:shadow">
-                        <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                          <img
-                            src={image.url}
-                            alt={product.title}
-                            className="w-full h-full object-center object-cover group-hover:opacity-75"
-                          />
-                        </div>
-                        <h3 className="mt-4 text-lg font-thin text-black">
-                          {product.title}
-                        </h3>
-                        <p className="mt-1 text-lg font-body text-black">
-                          {formatPrice(parseInt(price.amount))}
-                        </p>
-                      </a>
-                    </Link>
-                  );
-                })}
-              </div>
+        <div>
+          <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {data.data.products.edges.map((item: Edge) => {
+                const product = item.node;
+                const image = product.featuredImage;
+                const price = product.priceRange.minVariantPrice;
+                return (
+                  <Link
+                    key={product.handle}
+                    href={`/product/${product.handle}`}
+                  >
+                    <a className="group border-b-2 bg-teal-50 rounded-md hover:shadow">
+                      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                        <img
+                          src={image.url}
+                          alt={product.title}
+                          className="w-full h-full object-center object-cover group-hover:opacity-75"
+                        />
+                      </div>
+                      <h3 className="mt-4 text-lg font-thin text-black">
+                        {product.title}
+                      </h3>
+                      <p className="mt-1 text-lg font-body text-black">
+                        {formatPrice(parseInt(price.amount))}
+                      </p>
+                    </a>
+                  </Link>
+                );
+              })}
             </div>
           </div>
-        </main>
+        </div>
       </div>
       <Footer />
     </div>
