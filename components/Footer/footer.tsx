b22e5221f2fe3604158baef59f7a99303f4230dd
Jdure <22172148+Jdure@users.noreply.github.com>
2023-02-24 13:58:43 -0500

    fixed footer

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -2,13 +2,13 @@ export default function Footer() {
   return (
     <footer className="text-hover body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <span className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
           <span className="ml-3 text-xl">🧵 Nouveau Threads</span>
-        </a>
+        </span>
         <p className="text-sm text-neutral sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           {`© ${new Date().getFullYear()} Nouveau Threads`} —
           <a
-            href=""
+            href="/"
             className="text-hover ml-1"
             rel="noopener noreferrer"
             target="_blank"
@@ -17,7 +17,7 @@ export default function Footer() {
           </a>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-neutral">
+          <span className="text-neutral">
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -28,8 +28,8 @@ export default function Footer() {
             >
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
             </svg>
-          </a>
-          <a className="ml-3 text-neutral">
+          </span>
+          <span className="ml-3 text-neutral">
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -40,8 +40,8 @@ export default function Footer() {
             >
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
             </svg>
-          </a>
-          <a className="ml-3 text-neutral">
+          </span>
+          <span className="ml-3 text-neutral">
             <svg
               fill="none"
               stroke="currentColor"
@@ -54,7 +54,7 @@ export default function Footer() {
               <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
             </svg>
-          </a>
+          </span>
         </span>
       </div>
     </footer>
